01dc9c04b46ea82a8c922688d87825d4a5f0ce4b
Nimish.go <nimishgodbole409@gmail.com>
2025-03-12 16:11:34 +0530

    Added Modal and did some styling

--- Javascript/Project_1_Library/index.js
+++ Javascript/Project_1_Library/index.js
@@ -1,30 +1,56 @@
 const Library = [];
 
-const Book = (id, name, price, dop, genre) => {
+const Book = (id, name, genre) => {
     this.id = id;
     this.name = name;
-    this.price = price;
-    this.dop = dop;
     this.genre = genre;
 }
 
-const addBookToLibrary = () => {
-    const name = document.getElementById('bookName');
-    const price = document.getElementById('bookPrice');
-    const dop = document.getElementById('bookDate');
-    const genre = document.getElementById('bookGenre');
+const addBook = document.querySelector('.add-new-book');
+
+const dialog = document.querySelector('dialog');
+
+addBook.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+const submitBook = document.querySelector(".submitBook");
+
+submitBook.addEventListener("click" , addBookToLibrary, false);
+
+const addBookToLibrary = (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('.bookName');
+    const genre = document.querySelector('.bookGenre');
     const id = crypto.randomUUID();
 
     const book = new Book(id,name,price,dop,genre);
-
     Library.push(book);
+
+    dialog.close();
+
 }
 
-const cards = document.getElementById('cards')
-Book.prototype.display = () => {
+const closeModal = document.getElementById("close");
+
+closeModal.addEventListener("click" , () => {
+    dialog.close();
+})
+
+const cards = document.getElementById('cards');
+
+if(Library.length === 0 || Library.length === null || Library.length === undefined){
     cards.innerHTML = `
     <div class='card'>
-        <h1 class='card-title'>${this.name}</h1>
-        <p class='book-genre'>${this.price}</p>
+        <p class='null-title'>No Books to be displayed</p>
     </div>`;
+}else{
+    Library.forEach((elem) => {
+        cards.innerHTML = `
+        <div class='card'>
+            <p class='card-title'>${elem.name}</p>
+            <p class='book-genre'>${elem.genre}</p>
+        </div>`;
+    });
 }
